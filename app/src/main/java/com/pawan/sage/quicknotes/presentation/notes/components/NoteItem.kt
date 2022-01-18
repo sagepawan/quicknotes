e9cc45c830f1dca1d39649f0ea9cb431e0ed87db
Pawan <pawan.dhami3451@gmail.com>
2022-01-18 23:46:58 +1100

    setup for canvas created that houses two round rects, one for parent note body and another for top left clip on each note

--- app/src/main/java/com/pawan/sage/quicknotes/presentation/notes/components/NoteItem.kt
+++ app/src/main/java/com/pawan/sage/quicknotes/presentation/notes/components/NoteItem.kt
@@ -1,10 +1,19 @@
 package com.pawan.sage.quicknotes.presentation.notes.components
 
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
 import com.pawan.sage.quicknotes.feature_note.domain.model.Note
 
 @Composable
@@ -15,5 +24,42 @@ fun NoteItem(
     cutCornerSize: Dp = 30.dp,
     onDeleteClick: () -> Unit
 ) {
+    Box(
+        modifier = modifier
+    ){
+        //matchParentSize gives sizes once parent dimensions are rendered
+        //fillMaxSize will end up affecting size of parent box itself
+
+        Canvas(modifier = Modifier.matchParentSize()){
+            val clipPath = Path().apply{
+                lineTo(size.width-cutCornerSize.toPx(), 0f)
+                lineTo(size.width, cutCornerSize.toPx())
+                lineTo(size.width, size.height)
+                lineTo(0f, size.height)
+                close()
+            }
+
+            clipPath(clipPath){
+                //to define the note rectangle
+                drawRoundRect(
+                    color = Color(note.color),
+                    size = size,
+                    cornerRadius = CornerRadius(cornerRadius.toPx())
+                )
+
+                //to define the note rectangle clip on top right of each note
+                drawRoundRect(
+                    //darker shade of parent note color
+                    color = Color(
+                        ColorUtils.blendARGB(note.color, 0x000000, 0.2f)
+                    ),
+                    topLeft = Offset(size.width - cutCornerSize.toPx(), -100f),
+                    size = Size(cutCornerSize.toPx()+100f, cutCornerSize.toPx()+100f),
+                    cornerRadius = CornerRadius(cornerRadius.toPx())
+                )
+            }
+        }
+
+    }
 
 }
