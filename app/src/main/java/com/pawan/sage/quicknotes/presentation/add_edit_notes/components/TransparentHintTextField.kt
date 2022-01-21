33a249e8445f83b5ff182ee4a7d4984ab38c22e7
Pawan <pawan.dhami3451@gmail.com>
2022-01-21 11:26:44 +1100

    composable function for transparent hint text field added

---
+++ app/src/main/java/com/pawan/sage/quicknotes/presentation/add_edit_notes/components/TransparentHintTextField.kt
@@ -0,0 +1,51 @@
+package com.pawan.sage.quicknotes.presentation.add_edit_notes.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun TransparentHintTextField(
+    text:String,
+    hint:String,
+    modifier: Modifier = Modifier,
+    isHintVisible: Boolean = true,
+    onValueChange: (String) -> Unit,
+    textStyle: TextStyle = TextStyle(),
+    singleLine: Boolean = false,
+    onFocusChange: (FocusState) -> Unit
+) {
+    Box(
+        modifier = modifier
+        //check which alignment definitions during tests
+        //contentAlignment = Alignment.CenterStart
+    ){
+        BasicTextField(
+            value = text,
+            onValueChange = onValueChange,
+            singleLine = singleLine,
+            textStyle = textStyle,
+            modifier = androidx.compose.ui.Modifier
+                .fillMaxWidth()
+                .onFocusChanged {
+                    onFocusChange(it)
+                }
+        )
+
+        if(isHintVisible){
+            Text(
+                text = hint,
+                style = textStyle,
+                color = Color.DarkGray
+            )
+        }
+    }
+}
